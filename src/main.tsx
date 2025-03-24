0f25ad0feb6deb55952d9f43806c9fbf3feece1f
lucianogasquess <luciano.gasques123@gmail.com>
2025-03-24 17:33:46 -0300

    adição speed Insighths do Vercel

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
+import { SpeedInsights } from '@vercel/speed-insights/react'; //  Adicionando Speed Insights
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <SpeedInsights /> {}
     <App />
   </StrictMode>
 );
